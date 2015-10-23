caf597cf82ecdfcf8f4dc7bd4a8ddd0d35d47ebe
Ruslan Abylkassov <ruslan.a@me.com>
2015-10-23 18:45:18 +1000

    pretty background

--- cms/index.php
+++ cms/index.php
@@ -1,10 +1,11 @@
-<?php if(isset($_GET['a']) && $_GET['a'] == 'login' OR !isset($_GET['a'])) { ?>
 <head>
 	<meta charset="UTF-8" />
-	<title>Tecuno Login</title>
+	<title>Tecuno</title>
 	<link rel="stylesheet" href="./cms.css">
 	<link rel="icon" type="image/png" href="/favicon.ico" />
 </head>
+
+<?php if(isset($_GET['a']) && $_GET['a'] == 'login' OR !isset($_GET['a'])) { ?>
 <body>
 	<div class="outerWrapper">
 	<div class="wrapper">
@@ -12,7 +13,7 @@
 			if (isset($_GET['wp']) && $_GET['wp'] == 1)
 				echo '<div class="heading">Wrong Password</div>';
 			else
-				echo '<div class="heading">Tecuno Login</div>';
+				echo '<div class="heading">Login</div>';
 		?>
 		<div class="form">
 			<form action="?a=newArticle" method="post">
@@ -23,22 +24,12 @@
 		</div>
 	</div>
 </body>
-<?php }
+<?php 
+}
+
 if(isset($_GET['a']) && $_GET['a'] == 'newArticle') { 
-if($_POST['password'] == 'test') {  // todo: fix this password placeholder shit
+ // todo: fix this password placeholder shit
 ?>
-<head>
-	<title>New Article</title>
-	<link rel="stylesheet" href="./cms.css">
-	<script type="text/javascript"> 
-		function stopRKey(evt) { // stop enter key submitting form
-		  var evt = (evt) ? evt : ((event) ? event : null); 
-		  var node = (evt.target) ? evt.target : ((evt.srcElement) ? evt.srcElement : null); 
-		  if ((evt.keyCode == 13) && (node.type=="text"))  {return false;} 
-		} 
-		document.onkeypress = stopRKey; 
-	</script>
-</head>
 <body>
 	<div class="outerWrapper">
 		<div class="wrapper newArticle">
@@ -58,87 +49,81 @@ if($_POST['password'] == 'test') {  // todo: fix this password placeholder shit
 			 </form>
 			</div>
 		</div>
-
-
 <?php
-	} else {
-		// echo '<meta http-equiv="REFRESH" content="0;url=/cms?wa=1">';
-	} 
-
 	if(isset($_GET['a']) && $_GET['a'] == 'newArticle') {  ?>
 	<div class="outerWrapper">
 		<div class="wrapper listArticles">
 			<div class="heading">All Articles</div>
 			<?php
-include 'db.php';
+				include 'db.php';
 
-$filter = NULL;
-$page = NULL;
-$noArticles = NULL;
+				$filter = NULL;
+				$page = NULL;
+				$noArticles = NULL;
 
-if(isset($_GET['filter'])) { 
-	$filter = $_GET['filter'];
-}
+				if(isset($_GET['filter'])) { 
+					$filter = $_GET['filter'];
+				}
 
-if(isset($_GET['page'])) {
-	$page = $_GET['page']-1;
-	if($_GET['page'] < 1) {
-		$page = 0;
-	}
-}
+				if(isset($_GET['page'])) {
+					$page = $_GET['page']-1;
+					if($_GET['page'] < 1) {
+						$page = 0;
+					}
+				}
 
-$rangeStart = $page * 5;
-$rangeEnd = $rangeStart + 5;
+				$rangeStart = $page * 5;
+				$rangeEnd = $rangeStart + 5;
 
 
-$query = ('SELECT * FROM articles ORDER BY created DESC LIMIT :rangeStart, :rangeEnd');
-$statement = $con -> prepare($query);
-$statement -> bindValue(':rangeStart', intval($rangeStart), PDO::PARAM_INT);
-$statement -> bindValue(':rangeEnd', intval($rangeEnd), PDO::PARAM_INT);
-if (!$statement -> execute()) {
-    print_r($statement->errorInfo());
-}
-$result = $statement -> fetchAll(PDO::FETCH_ASSOC);
-$counter = 0;
+				$query = ('SELECT * FROM articles ORDER BY created DESC LIMIT :rangeStart, :rangeEnd');
+				$statement = $con -> prepare($query);
+				$statement -> bindValue(':rangeStart', intval($rangeStart), PDO::PARAM_INT);
+				$statement -> bindValue(':rangeEnd', intval($rangeEnd), PDO::PARAM_INT);
+				if (!$statement -> execute()) {
+				    print_r($statement->errorInfo());
+				}
+				$result = $statement -> fetchAll(PDO::FETCH_ASSOC);
+				$counter = 0;
 
-foreach ($result as $row) {
-	$articles[$counter] = $row;
-	$counter += 1;
-}
+				foreach ($result as $row) {
+					$articles[$counter] = $row;
+					$counter += 1;
+				}
 
-echo "<div class='grid_8 postsContainer'>";
-if($filter == "videos") {
-	echo "<h2 class='postTitle' style='margin-top: 25px; text-align: center;'>Videos coming soon!</h2>";
-}
-	
-$reverseCount = 0;
-while($reverseCount < 5) {
-	$id = $articles[$reverseCount]['id'];
-	$url = $articles[$reverseCount]['url'];
-	$title =  htmlspecialchars($articles[$reverseCount]['title'], ENT_QUOTES);
-	$author = $articles[$reverseCount]['author'];
-	$created = $articles[$reverseCount]['created'];
-	$category = $articles[$reverseCount]['category'];
-	$type = $articles[$reverseCount]['type'];
-	$blurb = substr(file_get_contents('articles/'.$url.'/text.html',TRUE),0,200);
-	if($url == null) {
-		$noArticles = true;
-		break;
-	}
-	if(($filter == $type) or ($filter == null)) {
-		echo "<div class='grid_8 post'>";
-			echo "<a href='article.php?id=$id&amp;name=$url'><img class='postImage' src='articles/$url/banner.jpeg' alt='$title Image' width='100' style='float: left; margin: 10px;'/></a>";
-			echo "<h2 class='postTitle'><a href='article.php?id=$id&amp;name=$url'>$title</a></h2>";
-			echo $blurb."…";
-			echo " <a class='readMore' href='article.php?id=$id&amp;name=$url'>read more.</a></p>";
-			echo "<div class='postFooter'>
-				<span class='authorDate'> by $author on $created in <a href='/?filter=$type'>$type</a> </span>
-				<a class='commentCount button' href='?a=edit'>Edit</a>
-			</div>";
-		echo "</div>";
-	}
-	$reverseCount ++;
-}
+				echo "<div class='grid_8 postsContainer'>";
+				if($filter == "videos") {
+					echo "<h2 class='postTitle' style='margin-top: 25px; text-align: center;'>Videos coming soon!</h2>";
+				}
+					
+				$reverseCount = 0;
+				while($reverseCount < 5) {
+					$id = $articles[$reverseCount]['id'];
+					$url = $articles[$reverseCount]['url'];
+					$title =  htmlspecialchars($articles[$reverseCount]['title'], ENT_QUOTES);
+					$author = $articles[$reverseCount]['author'];
+					$created = $articles[$reverseCount]['created'];
+					$category = $articles[$reverseCount]['category'];
+					$type = $articles[$reverseCount]['type'];
+					$blurb = substr(file_get_contents('articles/'.$url.'/text.html',TRUE),0,200);
+					if($url == null) {
+						$noArticles = true;
+						break;
+					}
+					if(($filter == $type) or ($filter == null)) {
+						echo "<div class='grid_8 post'>";
+							echo "<a href='article.php?id=$id&amp;name=$url'><img class='postImage' src='articles/$url/banner.jpeg' alt='$title Image' width='100' style='float: left; margin: 10px;'/></a>";
+							echo "<h2 class='postTitle'><a href='article.php?id=$id&amp;name=$url'>$title</a></h2>";
+							echo $blurb."…";
+							echo " <a class='readMore' href='article.php?id=$id&amp;name=$url'>read more.</a></p>";
+							echo "<div class='postFooter'>
+								<span class='authorDate'> by $author on $created in <a href='/?filter=$type'>$type</a> </span>
+								<a class='commentCount button' href='?a=edit'>Edit</a>
+							</div>";
+						echo "</div>";
+					}
+					$reverseCount ++;
+				}
 
 
 ?>
